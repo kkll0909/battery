cef35d3bc464b63bec2f0c52198bfe14469bac26
llkk <140357530+kkll0909@users.noreply.github.com>
2025-05-06 12:17:59 +0800

    更改

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -7,13 +7,6 @@ use app\common\controller\Frontend;
 class Index extends Frontend
 {
 
-    protected $noNeedLogin = '*';
-    protected $noNeedRight = '*';
-    protected $layout = '';
-
-    public function index()
-    {
-        return $this->view->fetch();
-    }
+    
 
 }
