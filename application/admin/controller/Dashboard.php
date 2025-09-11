0947bcee426411fabfc1769867daa9d2227c4e2e
kkll0909 <140357530+kkll0909@users.noreply.github.com>
2025-09-11 08:18:53 +0800

    优化

--- application/admin/controller/Dashboard.php
+++ application/admin/controller/Dashboard.php
@@ -84,4 +84,65 @@ class Dashboard extends Backend
         return $this->view->fetch();
     }
 
+    /**
+     * 查看
+     */
+    public function mdindex()
+    {
+        try {
+            \think\Db::execute("SET @@sql_mode='';");
+        } catch (\Exception $e) {
+
+        }
+        $column = [];
+        $starttime = Date::unixtime('day', -6);
+        $endtime = Date::unixtime('day', 0, 'end');
+        $joinlist = Db("user")->where('jointime', 'between time', [$starttime, $endtime])
+            ->field('jointime, status, COUNT(*) AS nums, DATE_FORMAT(FROM_UNIXTIME(jointime), "%Y-%m-%d") AS join_date')
+            ->group('join_date')
+            ->select();
+        for ($time = $starttime; $time <= $endtime;) {
+            $column[] = date("Y-m-d", $time);
+            $time += 86400;
+        }
+        $userlist = array_fill_keys($column, 0);
+        foreach ($joinlist as $k => $v) {
+            $userlist[$v['join_date']] = $v['nums'];
+        }
+
+        $dbTableList = Db::query("SHOW TABLE STATUS");
+        $addonList = get_addon_list();
+        $totalworkingaddon = 0;
+        $totaladdon = count($addonList);
+        foreach ($addonList as $index => $item) {
+            if ($item['state']) {
+                $totalworkingaddon += 1;
+            }
+        }
+        $this->view->assign([
+            'totaluser'         => User::count(),
+            'totalrevenue'        => Orderpay::where(['isy'=>1,'paystatus'=>'pay'])->sum('paymoney'),
+            'totalshops'        => Shop::count(),
+            'totalcategory'     => \app\common\model\Category::count(),
+            'todayusersignup'   => User::whereTime('jointime', 'today')->count(),
+            'todayuserlogin'    => User::whereTime('logintime', 'today')->count(),
+            'sevendau'          => User::whereTime('jointime|logintime|prevtime', '-7 days')->count(),
+            'thirtydau'         => User::whereTime('jointime|logintime|prevtime', '-30 days')->count(),
+            'threednu'          => User::whereTime('jointime', '-3 days')->count(),
+            'sevendnu'          => User::whereTime('jointime', '-7 days')->count(),
+            'investornums'       => 0,
+            'investorreturns'    => 0,
+            'todayorders' => Cgorders::where(['admin_id'=>$this->auth->id])->whereTime('stime','d')->where(['status'=>'pay'])->count(),
+            'nextmonthmoney'    => Orderpay::whereBetween('paydate',[time(),strtotime('+1 month')])->where(['isy'=>1,'paystatus'=>'nopay'])->sum('paymoney'),
+            'revenueday'    => Orderpay::whereTime('paydate','d')->where(['isy'=>1,'paystatus'=>'pay'])->sum('paymoney'),
+            'revenuemonth'    => Orderpay::whereTime('paydate','m')->where(['isy'=>1,'paystatus'=>'pay'])->sum('paymoney'),
+            'devicesidle'       => Bat::where(['admin_id'=>$this->auth->id])->count(),
+            'devicesuse'       => Db::table('fa_viewbatx')->where(['admin_id'=>$this->auth->id])->count(),
+        ]);
+
+        $this->assignconfig('column', array_keys($userlist));
+        $this->assignconfig('userdata', array_values($userlist));
+
+        return $this->view->fetch();
+    }
 }
